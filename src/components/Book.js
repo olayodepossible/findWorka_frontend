bcff14f3679ded0a75637ed99180fedf9bef6629
possible-gratitude <abayomi.gratitudeindia@gmail.com>
2020-09-13 13:36:13 +0100

    project setup

---
+++ src/components/Book.js
@@ -0,0 +1,13 @@
+import React from "react";
+import BookCard from "./BookCard";
+
+const Book = () => {
+  return (
+    <div>
+      {" "}
+      <BookCard />
+    </div>
+  );
+};
+
+export default Book;
---
+++ src/components/BookCard.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const BookCard = () => {
+  return (
+    <div className="card">
+      {" "}
+      <div>
+        <img src="./assets/game_throne.jpg" alt="" />
+      </div>{" "}
+      <h3>tiltle</h3>
+      <p>released date</p>
+    </div>
+  );
+};
+
+export default BookCard;
