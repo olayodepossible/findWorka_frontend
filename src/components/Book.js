db12f6dc3ccd586b496f4f3bc67f6f914bfa4aac
possible-gratitude <abayomi.gratitudeindia@gmail.com>
2020-09-14 11:51:47 +0100

    project setup

--- src/components/Book.js
+++ src/components/Book.js
@@ -3,11 +3,23 @@ import BookCard from "./BookCard";
 
 const Book = () => {
   return (
-    <div>
+    <div style={cardStyle}>
       {" "}
+      <BookCard />
+      <BookCard />
+      <BookCard />
+      <BookCard />
+      <BookCard />
       <BookCard />
     </div>
   );
 };
 
 export default Book;
+
+const cardStyle = {
+  display: "grid",
+  gridTemplateColumns: "repeat(3, 400px)",
+  margin: "0 150px",
+  width: "50px",
+};
--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -1,16 +1,43 @@
 import React from "react";
+import { CommentOutlined, UsergroupAddOutlined } from "@ant-design/icons";
 
 const BookCard = () => {
   return (
-    <div className="card">
+    <div className="card" style={bookCard}>
       {" "}
       <div>
         <img src="./assets/game_throne.jpg" alt="" />
       </div>{" "}
-      <h3>tiltle</h3>
-      <p>released date</p>
+      <h3>Tiltle: {}</h3>
+      <h4>Author: {}</h4>
+      <h4>Released date: {}</h4>
+      <div style={{ position: "relative" }}>
+        <span className="count" id="comment" style={count}>
+          {} 3
+        </span>
+        <CommentOutlined style={{ fontSize: "30px", marginRight: "20px" }} />
+        <span className="count" id="character" style={count}>
+          {} 5
+        </span>
+        <UsergroupAddOutlined style={{ fontSize: "30px" }} />
+      </div>
     </div>
   );
 };
 
 export default BookCard;
+
+const bookCard = {
+  margin: "20px",
+  backgroundColor: "#fff",
+  padding: "20px 0 20px 0",
+};
+
+const count = {
+  position: "absolute",
+  backgroundColor: "red",
+  borderRadius: "50%",
+  color: "#fff",
+  top: "-10px",
+  width: " 20px",
+};
