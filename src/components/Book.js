9902fca22519366d6f4173f793470455287d3d9a
possible-gratitude <abayomi.gratitudeindia@gmail.com>
2020-09-14 15:47:12 +0100

    fetching data api

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,12 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { LoadingOutlined } from "@ant-design/icons";
+import axios from "axios";
 import BookCard from "./BookCard";
 
 const Book = () => {
+  const [books, setBooks] = useState();
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [bookPerPage, setBookPerPage] = useState(6);
+
+  useEffect(() => {
+    const fetchBooks = async () => {
+      try {
+        const { data } = await axios.get(
+          "https://findworka.herokuapp.com/api/books"
+        );
+        console.log(data);
+        setBooks(data);
+        setLoading(false);
+        return data;
+      } catch (error) {}
+    };
+
+    fetchBooks();
+  }, []);
+
+  if (loading) {
+    return (
+      <div style={{ margin: "50vh auto" }}>
+        <LoadingOutlined />
+      </div>
+    );
+  }
   return (
     <div style={cardStyle}>
-      {" "}
-      <BookCard />
-      <BookCard />
+      {/*books.map((book) => (
+        <BookCard book={book} />
+      ))*/}
+
       <BookCard />
       <BookCard />
       <BookCard />
